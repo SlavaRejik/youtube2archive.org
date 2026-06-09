162ce2f2456d0a957c52f778e80c75860d8d6e91
Slava Rejik <slava@rejik.ru>
2026-06-09 13:13:52 +0300

    Video instructions added

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 1. Install libs from sys/requirements.txt
 2. Create maria/mysql db by sys/oyvideo.sql
 3. Rename dist-config.py to config.py and write vars
+
+## Пошаговая видео инструкция:
+https://youtu.be/fbliD58xd6k
+
+https://archive.org/details/oyid_K9jwMHOw8e
--- dist-config.py
+++ dist-config.py
@@ -1,16 +1,16 @@
-youtube_dir = '~/disk-2025/youtube'
-log_dir = '~/disk-2025/logs'
+youtube_dir = '/disk-2025/youtube'
+log_dir = '/disk-2025/logs'
 
 mariadb_connect = {
     'user': 'oyvideo',
     'password': 'password',
-    'host': 'host.ru',
+    'host': '127.0.0.1',
     'port': 3306,
     'database': 'oyvideo'
 }
 
 
-# yt_dlp optinins: https://github.com/yt-dlp/yt-dlp#filesystem-options
+# yt_dlp options: https://github.com/yt-dlp/yt-dlp#filesystem-options
 #yt_dlp = 'yt-dlp --cookies ~/tmp/cookies.txt  --proxy="socks5://127.0.0.1:8888"'
 #yt_dlp = 'yt-dlp --cookies-from-browser chrome+basictext --proxy="socks5://127.0.0.1:8888"'
 yt_dlp = 'yt-dlp --cookies-from-browser firefox --proxy="socks5://127.0.0.1:8888"'
@@ -19,9 +19,7 @@ yt_dlp = 'yt-dlp --cookies-from-browser firefox --proxy="socks5://127.0.0.1:8888
 
 channel_id = 'UCR0-7hO2RMofHzerE8GcnxQ' # @OpenYoga108
 
-storage = 'rejik-2026'
+storage = 'test-2026'
 
 telegram_api_key = '123'
 telegram_chat_id = '123'
-
-YOUTUBE_API_KEY = '123'
